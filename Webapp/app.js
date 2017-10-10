d7fcf971dd917e02f633fb282df17b79693ab226
Zuhair Rayyes <zuz1995@yahoo.com>
2017-10-10 16:20:03 +0200

    Completed Webapp

--- Webapp/app.js
+++ Webapp/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const reload = require('reload');
 const app = express();
 const dataFile = require('./data/data.json');
+const io = require('socket.io')();
 
 app.set('port', process.env.PORT || 3000);
 app.set('appData', dataFile);
@@ -22,6 +23,15 @@ app.use(require('./routes/chat'));
 
 const server = app.listen(app.get('port'), ()=> {
     console.log(`Server started on port ${app.get('port')} ...`);
+});
+
+io.attach(server);
+io.on('connection',(socket)=> {
+    console.log('User connected');
+
+    socket.on('postMessage', (data) => {
+        io.emit('updateMessage', data);
+    })
 });
 
 reload(app);
--- Webapp/public/js/chat.js
+++ Webapp/public/js/chat.js
@@ -1,24 +1,39 @@
-const chartForm = document.forms.chatForm;
+const socket = io();
+let chatUsername = document.querySelector('#chat-username');
+let chatMessage = document.querySelector('#chat-message');
 
-if (chartForm) {
-    let chatUsername = document.querySelector('#chat-username');
-    let chatMessage = document.querySelector('#chat-message');
+socket.on('connect',() =>{
+    const chartForm = document.forms.chatForm;
 
-    chatForm.addEventListener('submit', (e)=>{
-        e.preventDefault();
-        showMessage({
-            username: chatUsername.value,
-            message: chatMessage.value
-        });
-        chatMessage.value='';
-        chatMessage.focus();
-    });
-}
+    if (chartForm) {
+        chatForm.addEventListener('submit', (e)=>{
+            e.preventDefault();
+
+            socket.emit('postMessage',{
+                username: chatUsername.value,
+                message: chatMessage.value
+            });
+            chatMessage.value='';
+            chatMessage.focus();
+        }); //chatform event
+
+        socket.on('updateMessage',(data)=>{
+            showMessage(data);
+        }); //update message
+
+    } //chatform
+});
 
 function showMessage(data){
     let chatDisplay = document.querySelector('.chat-display');
     let newMessage = document.createElement('p');
-    newMessage.className = 'bg-success chat-text';
+
+    if (chatUsername.value == data.username){
+        newMessage.className = 'bg-success chat-text';
+    } else {
+        newMessage.className = 'bg-info text-warning chat-text';
+    }
+
     newMessage.innerHTML = '<strong>'+ data.username +'</strong>: '+ data.message;
     chatDisplay.insertBefore(newMessage, chatDisplay.firstChild);
 }
