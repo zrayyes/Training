2c0a7325b03c65c7633a9b5a5fe53cbeb596e419
Zuhair Rayyes <zuz1995@yahoo.com>
2017-10-10 11:08:21 +0200

    Added public folder and static route

--- Webapp/routes/index.js
+++ Webapp/routes/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req,res)=>{
-    res.send(`<h1>Welcome</h1>
-                <p>Roux Academy Meetups put together artists from all walks of life</p>`);
+    res.send(`
+<link rel="stylesheet" href="css/style.css">
+<h1>Welcome</h1>
+    <img src="/images/misc/background.jpg" alt="background" style="height: 300px">
+        <p>Roux Academy Meetups put together artists from all walks of life</p>`);
 });
 
 module.exports = router;
--- Webapp/routes/speakers.js
+++ Webapp/routes/speakers.js
@@ -5,19 +5,27 @@ router.get('/speakers', (req,res)=>{
     let info = '';
     let dataFile = req.app.get('appData');
     dataFile.speakers.forEach(item => {
-        info += `<li>
+        info += `
       <h2>${item.name}</h2>
+      <img src="/images/speakers/${item.shortname}_tn.jpg" alt="speaker">
       <p>${item.summary}</p>
-    </li>`;
+    `;
     });
-    res.send(`<h1>Roux Academy Meetups</h1>${info}`);
+    res.send(`
+<link rel="stylesheet" href="/css/style.css">
+<h1>Roux Academy Meetups</h1>
+    <img src="/images/misc/background.jpg" alt="background" style="height: 300px">${info}`);
 });
 
 router.get('/speakers/:speakerid', (req,res)=>{
     let dataFile = req.app.get('appData');
     let speaker = dataFile.speakers[req.params.speakerid];
-    res.send(`<h1>${speaker.title}</h1>
+    res.send(`
+<link rel="stylesheet" href="/css/style.css">
+<h1>${speaker.title}</h1>
+    <img src="/images/misc/background.jpg" alt="background" style="height: 300px">
                 <h2>with ${speaker.name}</h2>
+                <img src="/images/speakers/${speaker.shortname}_tn.jpg" alt="speaker">
                     <p>${speaker.summary}</p>`);
 });
 
