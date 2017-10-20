6e5a8ec6341853e2547b18ba952affb0a6cd1d3d
Zuhair Rayyes <zuz1995@yahoo.com>
2017-10-20 22:17:09 +0200

    Added ngFor and ngIf

--- Angular/first app/app/component.app.ts
+++ Angular/first app/app/component.app.ts
@@ -2,7 +2,26 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'app',
-    template: '<h1>Artist Directory</h1>'
+    templateUrl: './partials/app.html'
 })
 
-export class AppComponent {}
+export class AppComponent {
+    name : string;
+    artists : any;
+
+    constructor() {
+        this.name = 'Zuhair Rayyes';
+        this.artists = [
+            {
+                name: 'Barot Bellingham',
+                school: 'Penn State'
+            },{
+                name: 'Jonathan Ferrar',
+                school: 'University of California'
+            },{
+                name: 'Hillary Post',
+                school: 'University of Florida'
+            }
+        ]
+    }
+}
