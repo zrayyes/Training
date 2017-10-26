f4a82dab7a1e1d5dbc2081ce06f6e9d849ea2f8c
Zuhair Rayyes <zuz1995@yahoo.com>
2017-10-26 16:54:32 +0200

    Added second component, html, css

--- Angular/Angular 2 Training/app/app.component.ts
+++ Angular/Angular 2 Training/app/app.component.ts
@@ -1,10 +1,8 @@
-import {Component} from "@angular/core";
+import { Component } from '@angular/core';
 
-@Component(
-    {
-        selector: 'mw-app',
-        template: '<h1>My App</h1>'
-    }
-)
-
-export class AppComponent{}
+@Component({
+  selector: 'mw-app',
+  templateUrl: 'app/app.component.html',
+  styleUrls: ['app/app.component.css']
+})
+export class AppComponent {}
