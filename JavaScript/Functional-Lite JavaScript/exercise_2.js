b469e9a8e46902e265001c5233787780fd698d04
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-11 19:09:16 +0100

    Completed exercise 2

---
+++ JavaScript/Functional-Lite JavaScript/exercise_2.js
@@ -0,0 +1,14 @@
+// Define foo so that it produces a function which remembers
+// only the first two arguments that were passed to foo, and
+// always adds them together.
+
+function foo(x,y){
+    return function (){
+        return x+y;
+    }
+}
+
+let x = foo(3,4);
+
+x();    // 7
+x();    // 7
