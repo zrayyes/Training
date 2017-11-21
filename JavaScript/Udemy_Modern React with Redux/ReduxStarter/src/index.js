9ef1e40867c8d1e36d07762a1371aec92e91559f
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-21 12:17:00 +0100

    Successfully added to the DOM

--- JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/index.js
+++ JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/index.js
@@ -7,4 +7,4 @@ const App = function () {
 };
 
 // Take this component's HTML and put it on the page (in the DOM)
-ReactDOM.render(App);
+ReactDOM.render(<App/>, document.querySelector('.container'));
