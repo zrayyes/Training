a38cd6f9eec6fcc1a5f5096b39851d4696402833
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-21 22:20:19 +0100

    Changed App component to class based

--- JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/index.js
+++ JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 
@@ -10,14 +10,16 @@ YTSearch({key: API_KEY, term: "pokemon"}, data => {
     console.log(data);
 });
 
-// Create a  new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+
+class App extends Component {
+    render () {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 };
 
-// Take this component"s HTML and put it on the page (in the DOM)
+
 ReactDOM.render(<App/>, document.querySelector(".container"));
