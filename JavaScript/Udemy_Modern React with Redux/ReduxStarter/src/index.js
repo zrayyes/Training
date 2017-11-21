2fb7c71beaed6ccfa39efa8c6b689d5a2478523c
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-21 14:10:32 +0100

    Added youtube API key and search package

--- JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/index.js
+++ JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+
+const API_KEY = "AIzaSyDwa2mgGqHl_KPPWhXN8W-p38vuaz5c3L4";
 
 // Create a  new component. This component should produce some HTML
 const App = () => {
     return <div>Hi!</div>;
 };
 
-// Take this component's HTML and put it on the page (in the DOM)
-ReactDOM.render(<App/>, document.querySelector('.container'));
+// Take this component"s HTML and put it on the page (in the DOM)
+ReactDOM.render(<App/>, document.querySelector(".container"));
