990ce3939ed1aefefc6f4e4f3944ea3807c2c2f9
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-21 22:07:00 +0100

    Added default value to search bar input

--- JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/components/search_bar.js
+++ JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/components/search_bar.js
@@ -11,8 +11,9 @@ class SearchBar extends Component{
     render() {
         return (
             <div>
-                <input type="text" onChange={event => this.setState({ term: event.target.value })} />
-                <p>Value of the input: {this.state.term}</p>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({ term: event.target.value })} />
             </div>
         )
     }
