6366f0d7b4bbb73b46f273e7a1a2c4cd8374b757
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-21 16:07:10 +0100

    Added event listener to search bar

--- JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/components/search_bar.js
+++ JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/components/search_bar.js
@@ -3,8 +3,9 @@ import React, {Component} from "react";
 
 class SearchBar extends Component{
     render() {
-        return (<input type="text"/>)
+        return (<input type="text" onChange={event => console.log(event.target.value)}/>)
     }
+
 }
 
 export default SearchBar;
