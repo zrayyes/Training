00c32860047d7089792856dac811869c84a1c13b
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-21 22:15:52 +0100

    Succesfully made request with YouTube API

--- JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/components/search_bar.js
+++ JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/components/search_bar.js
@@ -13,7 +13,8 @@ class SearchBar extends Component{
             <div>
                 <input
                     value={this.state.term}
-                    onChange={event => this.setState({ term: event.target.value })} />
+                    onChange={event => this.setState({ term: event.target.value })}
+                />
             </div>
         )
     }
--- JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/index.js
+++ JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
 
 import SearchBar from "./components/search_bar";
 
 const API_KEY = "AIzaSyDwa2mgGqHl_KPPWhXN8W-p38vuaz5c3L4";
+
+YTSearch({key: API_KEY, term: "pokemon"}, data => {
+    console.log(data);
+});
 
 // Create a  new component. This component should produce some HTML
 const App = () => {
