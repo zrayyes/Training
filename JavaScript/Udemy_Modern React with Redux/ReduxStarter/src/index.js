28313413d5919d5d738bebb9a9e8f47157bc247e
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-21 22:28:08 +0100

    Added App component constructor

--- JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/index.js
+++ JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/index.js
@@ -6,12 +6,19 @@ import SearchBar from "./components/search_bar";
 
 const API_KEY = "AIzaSyDwa2mgGqHl_KPPWhXN8W-p38vuaz5c3L4";
 
-YTSearch({key: API_KEY, term: "pokemon"}, data => {
-    console.log(data);
-});
-
 
 class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {videos: []};
+
+        YTSearch({key: API_KEY, term: "pokemon"}, videos => {
+            this.setState({videos});
+            // equal to this.setState({ videos:videos });
+        });
+    }
+
     render () {
         return (
             <div>
