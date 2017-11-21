6d5aea16f21e42b2133524bdaf0908764e1c4895
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-21 21:46:53 +0100

    Added constructor for Searchbar

--- JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/components/search_bar.js
+++ JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/components/search_bar.js
@@ -2,10 +2,15 @@ import React, {Component} from "react";
 // same as using: const Component = React.Component;
 
 class SearchBar extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {term:""};
+    }
+
     render() {
         return (<input type="text" onChange={event => console.log(event.target.value)}/>)
     }
-
 }
 
 export default SearchBar;
