8f966522ca7540ffc9ce16f65f3c4ef6fafe8bc4
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-22 13:37:17 +0100

    Added video detail component

--- JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/index.js
+++ JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/index.js
@@ -4,6 +4,7 @@ import YTSearch from "youtube-api-search";
 
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
 const API_KEY = "AIzaSyDwa2mgGqHl_KPPWhXN8W-p38vuaz5c3L4";
 
