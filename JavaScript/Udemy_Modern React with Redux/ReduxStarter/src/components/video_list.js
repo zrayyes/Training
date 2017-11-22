1199db72cf896981574d7bb56fd3b8d34b85936f
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-22 12:44:20 +0100

    Added video list item

--- JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/components/video_list.js
+++ JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/components/video_list.js
@@ -1,10 +1,15 @@
 import React from "react";
+import VideoListItem from "./video_list_item";
 
 const VideoList = (props) => {
+    const videoItems = props.videos.map((video) => {
+        return <VideoListItem video={video}/>;
+    });
+
     return (
       <div>
           <ul className="col-md-4 list-group">
-              {props.videos.length}
+              {videoItems}
           </ul>
       </div>
     );
