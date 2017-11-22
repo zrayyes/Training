6e1025110b0020c13d8ee59432a66bd34bf86b41
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-22 12:29:40 +0100

    Added function to video list component

---
+++ JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/components/video_list.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const VideoList = () => {
+    return (
+      <div>
+          <ul className="col-md-4 list-group">
+          </ul>
+      </div>
+    );
+};
+
+export default VideoList;
