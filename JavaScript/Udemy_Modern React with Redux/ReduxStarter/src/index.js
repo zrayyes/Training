bfcb2a9590139cb088e8241e267f126cb30afa8c
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-22 13:59:52 +0100

    videolistitem now able to select and show video

--- JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/index.js
+++ JavaScript/Udemy_Modern React with Redux/ReduxStarter/src/index.js
@@ -13,11 +13,16 @@ class App extends Component {
     constructor(props){
         super(props);
 
-        this.state = {videos: []};
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
-        YTSearch({key: API_KEY, term: "pokemon"}, videos => {
-            this.setState({videos});
-            // equal to this.setState({ videos:videos });
+        YTSearch({key: API_KEY, term: "digimon"}, videos => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
@@ -25,12 +30,14 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoDetail video={ this.state.videos[0] }/>
-                <VideoList videos={ this.state.videos } />
+                <VideoDetail video={ this.state.selectedVideo }/>
+                <VideoList
+                    onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+                    videos={ this.state.videos } />
             </div>
         );
     }
-};
+}
 
 
 ReactDOM.render(<App/>, document.querySelector(".container"));
