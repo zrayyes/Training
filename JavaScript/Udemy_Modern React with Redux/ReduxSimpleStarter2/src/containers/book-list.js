1e7236e616cd710aeab17dfb548120fbff80b45d
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-24 20:53:16 +0100

    added selectBook method to BookList

--- JavaScript/Udemy_Modern React with Redux/ReduxSimpleStarter2/src/containers/book-list.js
+++ JavaScript/Udemy_Modern React with Redux/ReduxSimpleStarter2/src/containers/book-list.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
+import {selectBook} from "../actions/index";
+import {bindActionCreators} from "redux";
 
 class BookList extends Component {
     renderList(){
@@ -22,10 +24,21 @@ class BookList extends Component {
 }
 
 function mapStateToProps(state) {
-    // whatever is returned will show up as props inside BookList
+    // whatever is returned will show up as
+    // props inside BookList
     return {
         books: state.books
     };
 }
 
-export default connect(mapStateToProps)(BookList);
+// Anything returned from this function will end up as props
+// on the BookList container
+function mapDispatchToProps(dispatch) {
+    // Whenever selectBook is called, the result should be passed
+    // to all reducers
+    return bindActionCreators({ selectBook: selectBook}, dispatch);
+}
+
+// Promote BookList from a component to a container - it needs to know
+// about this new dispatch method, selectbook. Make it available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
