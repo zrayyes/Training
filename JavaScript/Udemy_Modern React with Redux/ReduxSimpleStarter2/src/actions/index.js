40765c210debc9f336e04b6a545c808b4b9f200b
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-27 19:06:55 +0100

    Index now returns action

--- JavaScript/Udemy_Modern React with Redux/ReduxSimpleStarter2/src/actions/index.js
+++ JavaScript/Udemy_Modern React with Redux/ReduxSimpleStarter2/src/actions/index.js
@@ -1,3 +1,8 @@
-function selectBook(book) {
-    console.log(`A book has been created, ${book.title}`);
+export function selectBook(book) {
+    // selectBook is an ActionCreator, it needs to return an action,
+    // an object with a type property
+    return {
+        type: "BOOK_SELECTED",
+        payload: book
+    };
 }
