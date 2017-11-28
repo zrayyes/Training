427cc227ff1b48f066086f2acca003fb63cb6194
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-28 12:40:19 +0100

    Added active book reducer

--- JavaScript/Udemy_Modern React with Redux/ReduxSimpleStarter2/src/reducers/index.js
+++ JavaScript/Udemy_Modern React with Redux/ReduxSimpleStarter2/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import BooksReducer from "./reducer_books";
+import ActiveBookReducer from "./reducer_active_book";
 
 const rootReducer = combineReducers({
     books: BooksReducer
