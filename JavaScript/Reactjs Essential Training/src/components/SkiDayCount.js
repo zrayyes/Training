02094cf7be95a219c0ee4d04071f5f0c416c4713
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-29 22:22:34 +0100

    Refactored class component into a stateless functional component

--- JavaScript/Reactjs Essential Training/src/components/SkiDayCount.js
+++ JavaScript/Reactjs Essential Training/src/components/SkiDayCount.js
@@ -1,34 +1,31 @@
-import React,{Component} from "react";
+import {Component} from "react";
 import "../stylesheets/ui.scss";
 
-export class SkiDayCount extends Component{
+const percentToDecimal = (decimal) => {
+    return (decimal*100+' %');
+};
 
-    percentToDecimal(decimal){
-        return (decimal*100+' %');
-    }
-    calcGoalProgress(total,goal){
-        return this.percentToDecimal(total/goal);
-    }
+const calcGoalProgress = (total,goal) => {
+    return percentToDecimal(total/goal);
+};
 
-   render(){
-       return (
-           <div className="ski-day-count">
-               <div className="total-days">
-                   <span>{this.props.total}</span>
-                   <span>days</span>
-               </div>
-               <div className="powder-days">
-                   <span>{this.props.powder}</span>
-                   <span>days</span>
-               </div>
-               <div className="backcountry-days">
-                   <span>{this.props.backcountry}</span>
-                   <span>days</span>
-               </div>
-               <div>
-                   <span>{this.calcGoalProgress(this.props.total,this.props.goal)}</span>
-               </div>
-           </div>
-       );
-   }
-}
+export const SkiDayCount = ({total, powder, backcountry, goal}) => (
+
+    <div className="ski-day-count">
+        <div className="total-days">
+            <span>{total}</span>
+            <span>days</span>
+        </div>
+        <div className="powder-days">
+            <span>{powder}</span>
+            <span>days</span>
+        </div>
+        <div className="backcountry-days">
+            <span>{backcountry}</span>
+            <span>days</span>
+        </div>
+        <div>
+            <span>{calcGoalProgress(total, goal)}</span>
+        </div>
+    </div>
+);
