7e0a1070277a48f44c4758b65f57448ef8ca6a3d
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-30 18:49:15 +0100

    Added form code to AddDayForm

--- JavaScript/Reactjs Essential Training/src/components/AddDayForm.js
+++ JavaScript/Reactjs Essential Training/src/components/AddDayForm.js
@@ -1,3 +1,21 @@
-export const AddDayForm = () => (
-    <h1>Add A Day</h1>
-);
+import { PropTypes, Component } from "react";
+
+export class AddDayForm extends Component {
+    render(){
+        return (
+            <form className="add-day-form">
+                <input type="text" id="resort" required/>
+                <input type="date" id="date" required/>
+                <input type="checkbox" id="powder" required/>
+                <input type="checkbox" id="backcountry" required/>
+            </form>
+        )
+    };
+}
+
+AddDayForm.propTypes = {
+    resort: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    powder: PropTypes.bool.isRequired,
+    backcountry: PropTypes.bool.isRequired
+};
