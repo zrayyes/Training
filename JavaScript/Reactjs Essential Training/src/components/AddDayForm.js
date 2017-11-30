b0bfdf10222f89e56d2466d04df0bb602edca031
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-30 19:10:27 +0100

    Added ref and submit function to form

--- JavaScript/Reactjs Essential Training/src/components/AddDayForm.js
+++ JavaScript/Reactjs Essential Training/src/components/AddDayForm.js
@@ -2,22 +2,38 @@ import { PropTypes, Component } from "react";
 
 export class AddDayForm extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.submit = this.submit.bind(this);
+    }
+
+    submit(e) {
+        e.preventDefault();
+        console.log('resort', this.refs.resort.value);
+        console.log('date', this.refs.date.value);
+        console.log('powder', this.refs.powder.value);
+        console.log('backcountry', this.refs.backcountry.value);
+    }
+
     render(){
 
         const {resort, date, powder, backCountry} = this.props;
 
         return (
-            <form className="add-day">
+            <form onSubmit={this.submit} className="add-day">
                 <label htmlFor="resort">Resort Name</label>
                 <input type="text"
                        id="resort"
                        defaultValue={resort}
+                       ref="resort"
                        required/>
 
                 <label htmlFor="date">Date</label>
                 <input type="date"
                        id="date"
                        defaultValue={date}
+                       ref="date"
                        required/>
 
                 <div>
@@ -25,6 +41,7 @@ export class AddDayForm extends Component {
                         type="checkbox"
                         id="powder"
                         defaultChecked={powder}
+                        ref="powder"
                         required/>
                     <label htmlFor="powder">Powder Day</label>
                 </div>
@@ -33,10 +50,11 @@ export class AddDayForm extends Component {
                         type="checkbox"
                         id="backcountry"
                         defaultChecked={backCountry}
+                        ref="backcountry"
                         required/>
                     <label htmlFor="backcountry">Backcountry Day</label>
                 </div>
-
+                <button>Add Day</button>
             </form>
         )
     };
