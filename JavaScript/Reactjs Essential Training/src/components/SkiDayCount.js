441e825b21ff472c27f6d8f52df1dcf5829b4453
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-30 14:37:18 +0100

    Added PropTypes to SkiDayCount

--- JavaScript/Reactjs Essential Training/src/components/SkiDayCount.js
+++ JavaScript/Reactjs Essential Training/src/components/SkiDayCount.js
@@ -2,6 +2,7 @@ import "../stylesheets/ui.scss";
 import Terrain from 'react-icons/lib/md/terrain';
 import SnowFlake from 'react-icons/lib/ti/weather-snow';
 import Calendar from 'react-icons/lib/fa/calendar';
+import {PropTypes} from "react";
 
 
 const percentToDecimal = (decimal) => {
@@ -40,4 +41,11 @@ SkiDayCount.defaultProps = {
     powder: 10,
     backcountry: 15,
     goal: 75
+};
+
+SkiDayCount.propTypes = {
+    total: PropTypes.number,
+    powder: PropTypes.number,
+    backcountry: PropTypes.number,
+    goal: PropTypes.number
 };
