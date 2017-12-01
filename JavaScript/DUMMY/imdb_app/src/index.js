5a1cff9d8766cf1e2822cef0417a5fba2e5077ac
Zuhair Rayyes <zuz1995@yahoo.com>
2017-12-01 19:19:17 +0100

    Added base components

--- JavaScript/DUMMY/imdb_app/src/index.js
+++ JavaScript/DUMMY/imdb_app/src/index.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "./stylesheets/style.css";
 
-fetch('https://theimdbapi.org/api/find/movie?title=transformers&year=2007')
-    .then((response) => response.json())
-    .then((responseJson) => {
-        console.log(responseJson);
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+import {SearchBar} from "./components/SearchBar";
+
+window.React = React;
+
 
 render(
-    <h1>
-        Hello World
-    </h1>,
+    <div className="container">
+        <SearchBar/>
+    </div>,
     document.getElementById('react-container')
 );
