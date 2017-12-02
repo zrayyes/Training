3a809ea28351cc575f7da269b2c072657b85052c
Zuhair Rayyes <zuz1995@yahoo.com>
2017-12-02 18:13:30 +0100

    Divided SelectedItem component into columns

--- JavaScript/DUMMY/imdb_app/src/components/SelectedItem.js
+++ JavaScript/DUMMY/imdb_app/src/components/SelectedItem.js
@@ -2,10 +2,19 @@ export const SelectedItem = ({movie}) => {
     return (
         <div id="selected-movie">
             <h1>{movie.title}</h1>
-            <img src={movie.poster.large} className="img-fluid"/>
-            <h2>{movie.year}</h2>
-            <h4>{movie.genre}</h4>
-            <p>{movie.description}</p>
+            <div className="row">
+
+                <div className="col-md-4">
+                    <img src={movie.poster.large} className="img-fluid"/>
+                </div>
+
+                <div className="col-md-8">
+                    <h2>{movie.year}</h2>
+                    <h4>{movie.genre}</h4>
+                    <p>{movie.description}</p>
+                </div>
+
+            </div>
         </div>
     )
 };
