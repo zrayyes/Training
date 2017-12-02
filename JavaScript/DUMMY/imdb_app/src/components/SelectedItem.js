6309190d9b794b69a7dd51dc1b958d2d23fa506d
Zuhair Rayyes <zuz1995@yahoo.com>
2017-12-02 18:09:52 +0100

    Added basic movie data to SelectedItem component

--- JavaScript/DUMMY/imdb_app/src/components/SelectedItem.js
+++ JavaScript/DUMMY/imdb_app/src/components/SelectedItem.js
@@ -2,7 +2,10 @@ export const SelectedItem = ({movie}) => {
     return (
         <div id="selected-movie">
             <h1>{movie.title}</h1>
-
+            <img src={movie.poster.large} className="img-fluid"/>
+            <h2>{movie.year}</h2>
+            <h4>{movie.genre}</h4>
+            <p>{movie.description}</p>
         </div>
     )
 };
