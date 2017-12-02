da9077a044580467cbfea5c239d773784c24d354
Zuhair Rayyes <zuz1995@yahoo.com>
2017-12-02 23:27:31 +0100

    Fixed webpack config

--- JavaScript/DUMMY/imdb_app/webpack.config.js
+++ JavaScript/DUMMY/imdb_app/webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require("webpack");
 module.exports = {
   entry: __dirname +"/src/index.js",
   output: {
-      path: __dirname +"dist/assets",
+      path: __dirname +"/dist/assets",
       filename: "bundle.js",
       publicPath: "assets"
   },
