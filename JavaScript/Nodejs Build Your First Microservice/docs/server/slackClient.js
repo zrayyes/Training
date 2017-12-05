667edee9c5eb74491a5b60d1373cd172780c6e52
Zuhair Rayyes <zuz1995@yahoo.com>
2017-12-05 11:12:13 +0100

    slackClient now prints messages from slack

--- JavaScript/Nodejs Build Your First Microservice/docs/server/slackClient.js
+++ JavaScript/Nodejs Build Your First Microservice/docs/server/slackClient.js
@@ -2,6 +2,7 @@
 
 const RtmClient = require('@slack/client').RtmClient;
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
 function handleOnAuthenticated(rtmStartData) {
     console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
@@ -11,9 +12,14 @@ function addAuthenticatedHandler(rtm, handler) {
     rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, handler);
 }
 
+function handleOnMessage(message) {
+    console.log(message);
+}
+
 module.exports.init = function slackClient(bot_token, logLevel) {
     const rtm = new RtmClient(bot_token, {logLevel: logLevel});
     addAuthenticatedHandler(rtm, handleOnAuthenticated);
+    rtm.on(RTM_EVENTS.MESSAGE, handleOnMessage);
     return rtm;
 };
 
