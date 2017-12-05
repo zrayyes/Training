03e157044b17e37290ece3a28592dd3a1a3d87ea
Zuhair Rayyes <zuz1995@yahoo.com>
2017-12-05 15:38:42 +0100

    Added GET and POST requests

--- JavaScript/API Design in Nodejs/api-design-node-step-1/server/server.js
+++ JavaScript/API Design in Nodejs/api-design-node-step-1/server/server.js
@@ -1,27 +1,50 @@
 // TODO: make this work.
-// if yuo go to localhost:3000 the app
-// there is expected crud to be working here
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var _ = require('lodash');
 
-// express.static will serve everything
-// with in client as a static resource
-// also, it will server the index.html on the
-// root of that directory on a GET to '/'
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const _ = require('lodash');
+
 app.use(express.static('client'));
 
-// body parser makes it possible to post JSON to the server
-// we can accss data we post on as req.body
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
-var lions = [];
-var id = 0;
+let lions = [];
+let id = 0;
 
 // TODO: make the REST routes to perform CRUD on lions
+
+// GET
+app.get('/lions',(req,res) => {
+    res.json(lions);
+});
+
+// GET by ID
+app.get('/lions/:id',(req,res) => {
+    let id = Number(req.params.id);
+    selectedLion = lions.filter(lion => lion.id === id)[0];
+    res.json(selectedLion || {});
+});
+
+// POST
+app.post('/lions',(req,res) => {
+    newLion = req.body;
+    newLion.id = id;
+    lions.push(newLion);
+    id += 1;
+});
+
+// PUT
+app.put('/lions/:id',(req,res) => {
+    let id = Number(req.params.id);
+    selectedLion = lions.filter(lion => lion.id === id)[0];
+    res.json(lions);
+});
+
+// DESTROY
+
 
 app.listen(3000);
 console.log('on port 3000');
