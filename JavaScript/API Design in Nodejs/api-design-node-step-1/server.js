3126c2b82b6519262bd0393ca552521ea35f7f2c
Zuhair Rayyes <zuz1995@yahoo.com>
2017-12-05 12:32:15 +0100

    Returned basic json data

--- JavaScript/API Design in Nodejs/api-design-node-step-1/server.js
+++ JavaScript/API Design in Nodejs/api-design-node-step-1/server.js
@@ -2,4 +2,16 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
-var jsonData = {count: 12, message: 'hey'};
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+const jsonData = {count: 12, message: 'hey'};
+
+app.get('/',(req,res) => {
+    res.json(jsonData);
+});
+
+app.listen(port, ()=> {
+    console.log(`Server started on port ${port}`);
+});
