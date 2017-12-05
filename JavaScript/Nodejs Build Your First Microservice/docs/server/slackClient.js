5677bdd160bd6163b51a5599fa2cac42babd1eba
Zuhair Rayyes <zuz1995@yahoo.com>
2017-12-05 11:16:35 +0100

    Bot now sends back message to slack

--- JavaScript/Nodejs Build Your First Microservice/docs/server/slackClient.js
+++ JavaScript/Nodejs Build Your First Microservice/docs/server/slackClient.js
@@ -3,6 +3,7 @@
 const RtmClient = require('@slack/client').RtmClient;
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+let rtm = null;
 
 function handleOnAuthenticated(rtmStartData) {
     console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
@@ -14,10 +15,12 @@ function addAuthenticatedHandler(rtm, handler) {
 
 function handleOnMessage(message) {
     console.log(message);
+
+    rtm.sendMessage('This is a test message',message.channel);
 }
 
 module.exports.init = function slackClient(bot_token, logLevel) {
-    const rtm = new RtmClient(bot_token, {logLevel: logLevel});
+    rtm = new RtmClient(bot_token, {logLevel: logLevel});
     addAuthenticatedHandler(rtm, handleOnAuthenticated);
     rtm.on(RTM_EVENTS.MESSAGE, handleOnMessage);
     return rtm;
