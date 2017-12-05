e7557961623931108144ef15c66167635f6a5ff5
Zuhair Rayyes <zuz1995@yahoo.com>
2017-12-05 11:06:37 +0100

    Added slackClient service api

---
+++ JavaScript/Nodejs Build Your First Microservice/docs/server/slackClient.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const RtmClient = require('@slack/client').RtmClient;
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+
+function handleOnAuthenticated(rtmStartData) {
+    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+}
+
+function addAuthenticatedHandler(rtm, handler) {
+    rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, handler);
+}
+
+module.exports.init = function slackClient(bot_token, logLevel) {
+    const rtm = new RtmClient(bot_token, {logLevel: logLevel});
+    addAuthenticatedHandler(rtm, handleOnAuthenticated);
+    return rtm;
+};
+
+module.exports.addAuthenticatedHandler = addAuthenticatedHandler;
