a0b2f802840e9c5d86b7e0e4beb4da162a69ad87
Zuhair Rayyes <zuz1995@yahoo.com>
2017-12-09 13:18:15 +0100

    Added sql query

--- Databases/Learning SQL Programming/index.js
+++ Databases/Learning SQL Programming/index.js
@@ -1,5 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
+// Connect to the database
 let db = new sqlite3.Database('./Exercise Files/results.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
       return console.error(err.message);
@@ -7,15 +8,17 @@ let db = new sqlite3.Database('./Exercise Files/results.db', sqlite3.OPEN_READWR
     console.log('Connected to results database.');
 });
 
-db.serialize(() => {
-    db.each(`SELECT * FROM people`, (err, row) => {
-      if (err) {
-        console.error(err.message);
-      }
-      console.log(row);
-    });
-  });
 
+// SQL Query to execute
+let sqlQuery = "SELECT * FROM people";
+
+// Return SQL Query results
+db.all(sqlQuery, (err, results) => {
+    console.log(results)
+});
+
+
+// Close the connection
 db.close((err) => {
     if (err) {
       return console.error(err.message);
