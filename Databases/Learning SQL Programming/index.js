382ff3b9cfe1afe3d05550c5a359973e41661e1c
Zuhair Rayyes <zuz1995@yahoo.com>
2017-12-09 13:03:46 +0100

    Return results from db

--- Databases/Learning SQL Programming/index.js
+++ Databases/Learning SQL Programming/index.js
@@ -7,6 +7,15 @@ let db = new sqlite3.Database('./Exercise Files/results.db', sqlite3.OPEN_READWR
     console.log('Connected to results database.');
 });
 
+db.serialize(() => {
+    db.each(`SELECT * FROM people`, (err, row) => {
+      if (err) {
+        console.error(err.message);
+      }
+      console.log(row);
+    });
+  });
+
 db.close((err) => {
     if (err) {
       return console.error(err.message);
