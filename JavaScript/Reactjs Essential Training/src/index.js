e0e1ce3c592e37fb7a1dd48ff35970a5a5971561
Zuhair Rayyes <zuz1995@yahoo.com>
2017-12-13 20:10:54 +0100

    Implemented reducers in index

--- JavaScript/Reactjs Essential Training/src/index.js
+++ JavaScript/Reactjs Essential Training/src/index.js
@@ -1,11 +1,24 @@
 import C from "./constants";
-import challenge1 from "./challenges/CANCEL_FETCHING";
-import challenge2 from "./challenges/CHANGE_SUGGESTIONS";
-import challenge3 from "./challenges/CLEAR_SUGGESTIONS";
-import challenge4 from "./challenges/FETCH_RESORT_NAMES";
+import appReducer from "./store/reducers";
+import initialState from "./initialState.json";
 
+let state = initialState;
 
-console.log(challenge1);
-console.log(challenge2);
-console.log(challenge3);
-console.log(challenge4);
+console.log(JSON.stringify(state));
+
+state = appReducer(state,{
+    type: C.SET_GOAL,
+    payload: 2
+});
+
+state = appReducer(state,{
+    type: C.ADD_DAY,
+    payload: {
+        "resort": "Super Mountain",
+        "date": "2014-05-08",
+        "powder": false,
+        "backcountry": false
+    }
+});
+
+console.log(JSON.stringify(state));
