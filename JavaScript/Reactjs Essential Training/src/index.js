2ab7a456e872eb576c18c31fc1c1a8dc32ce7977
Zuhair Rayyes <zuz1995@yahoo.com>
2017-12-14 10:53:26 +0100

    Added unsubscribe mehtod

--- JavaScript/Reactjs Essential Training/src/index.js
+++ JavaScript/Reactjs Essential Training/src/index.js
@@ -2,15 +2,17 @@ import C from "./constants";
 import appReducer from "./store/reducers";
 import {createStore} from "redux";
 
-const initialState = (localStorage['redux-store']) ?
-                        JSON.parse(localStorage['redux-store']):
-                        {}
+const store = createStore(appReducer);
 
-const store = createStore(appReducer, initialState);
+const unsubscribeGoalLogger = store.subscribe(
+    ()=> console.log(` Goal: ${store.getState().goal}`)
+);
 
-store.subscribe(()=>(console.log(store.getState())));
+setInterval(()=> {
+    store.dispatch({
+        type: C.SET_GOAL,
+        payload: parseInt(Math.random() * 100)
+    })
+}, 500);
 
-store.subscribe(()=>{
-    const state = JSON.stringify(store.getState());
-    localStorage['redux-store'] = state;
-});
+setTimeout(()=> unsubscribeGoalLogger(), 3000);
