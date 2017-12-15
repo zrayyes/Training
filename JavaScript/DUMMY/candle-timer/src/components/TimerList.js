a737ac3bc6774276a4ecbbc0071e414d67803883
Zuhair Rayyes <zuz1995@yahoo.com>
2017-12-15 10:48:05 +0100

    Moved Timer to seperate repository

--- JavaScript/DUMMY/candle-timer/src/components/TimerList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {TimerItem} from "./TimerItem";
-
-
-export const TimerList = () => (
-    <ul>
-        <TimerItem/>
-    </ul>
-)
--- JavaScript/DUMMY/candle-timer/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './stylesheets/index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
