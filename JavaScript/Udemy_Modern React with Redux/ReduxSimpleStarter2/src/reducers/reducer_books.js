4c317ce589c87eedca3d0164d141038a75973aeb
Zuhair Rayyes <zuz1995@yahoo.com>
2018-01-08 23:41:55 +0100

    Removed old files

--- JavaScript/Udemy_Modern React with Redux/ReduxSimpleStarter2/src/reducers/reducer_books.js
+++
@@ -1,9 +0,0 @@
-export default function() {
-    return [
-        {title:"Javascript: The Good Parts"},
-        {title:"Harry Potter"},
-        {title:"The Lord of the Rings"},
-        {title:"Eloquent Ruby"},
-        {title:"Don't Make Me Think"}
-    ]
-}
