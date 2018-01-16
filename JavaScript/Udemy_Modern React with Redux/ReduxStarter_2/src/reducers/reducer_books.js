a2365a710c6fbf01f1b3558e75e62169817f77b9
Zuhair Rayyes <zuz1995@yahoo.com>
2018-01-16 21:39:30 +0100

    added books reducer

---
+++ JavaScript/Udemy_Modern React with Redux/ReduxStarter_2/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        {title: "Book 1"},
+        {title: "Book 2"},
+        {title: "Book 3"},
+        {title: "Book 4"},
+        {title: "Book 5"}
+    ]
+}
