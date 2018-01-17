5463a2b6ddf8d5f3c157f19e9f5980833a9932dd
Zuhair Rayyes <zuz1995@yahoo.com>
2018-01-17 23:44:53 +0100

    renamed booklist

--- JavaScript/Udemy_Modern React with Redux/ReduxStarter_2/src/components/app.js
+++ JavaScript/Udemy_Modern React with Redux/ReduxStarter_2/src/components/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import BookList from '../containers/book-list';
+import BookList from '../containers/BookList';
 
 export default class App extends Component {
   render() {
