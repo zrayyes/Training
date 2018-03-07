09fa640049df75b1b6b0e013bdc81f029f9a233b
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-03-07 16:39:34 +0100

    Added delete reducer

--- JavaScript/Udemy_Modern React with Redux/ReduxStarter_2/src/reducers/reducer_books.js
+++ JavaScript/Udemy_Modern React with Redux/ReduxStarter_2/src/reducers/reducer_books.js
@@ -1,15 +1,17 @@
-export default function (state, action) {
+let defaultState = [
+    { title: "Book 1", pages: 412 },
+    { title: "Book 2", pages: 331 },
+    { title: "Book 3", pages: 124 },
+    { title: "Book 4", pages: 82 },
+    { title: "Book 5", pages: 99 }
+]
+
+export default function (state = defaultState, action) {
     switch (action.type) {
         case 'DELETE_BOOK':
-            console.log(action.payload.title)
-            return state;
+            let newState = state.filter(book => book !== action.payload);
+            return newState;
     }
 
-    return [
-        { title: "Book 1", pages: 412 },
-        { title: "Book 2", pages: 331 },
-        { title: "Book 3", pages: 124 },
-        { title: "Book 4", pages: 82 },
-        { title: "Book 5", pages: 99 }
-    ]
+    return state;
 }
