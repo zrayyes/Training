933d66b091d909371e1e9194604711ed74a50c39
Zuhair Rayyes <zuz1995@yahoo.com>
2018-10-13 15:10:27 +0200

    Completed challenge 1

--- C/dummy/main.c
+++ C/dummy/main.c
@@ -2,10 +2,17 @@
 
 int main()
 {
-    char input[64];
-    printf("Input long string:");
-    fgets(input,64,stdin);
-    puts(input);
+    float a, b;
+    printf("Please enter values a:");
+    scanf_s("%f", &a);
+    printf("\nPlease enter values b:");
+    scanf_s("%f", &b);
+    printf("a + b = %f\n", a + b);
+    printf("a - b = %f\n", a - b);
+    printf("a * b = %f\n", a * b);
+    printf("a / b = %f\n", a / b);
+    printf("a^b = %f\n", pow(a, b));
+    printf("sqrt a = %f, sqrt b = %f\n", sqrt(a), sqrt(b));
 
-    return(0);
+    return (0);
 }
