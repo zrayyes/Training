f6a5c3d87ed9c5948300c059930ef5d85a7b2d22
Zuhair Rayyes <rayyes95@gmail.com>
2018-11-04 19:01:43 +0100

    Completed challenge

--- C/advanced dummy/ch04.c
+++ C/advanced dummy/ch04.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
+#include <string.h>
 
-#define SIZE 10
-
-void print_arr(int *arr)
+int print_arr(int *arr, size_t nitems, size_t size)
 {
-    for (int n = 0; n < SIZE; n++)
+    printf("Array:\t%");
+    for (int n = 0; n < nitems; n++)
     {
         printf("%03d ", arr[n]);
     }
     printf("\n");
 }
 
-void bubble_sort(int *arr)
+void bubble_sort(int *arr, size_t nitems)
 {
-    for (int outer = 0; outer < SIZE; outer++)
+    for (int outer = 0; outer < nitems; outer++)
     {
-        for (int inner = outer + 1; inner < SIZE; inner++)
+        for (int inner = outer + 1; inner < nitems; inner++)
         {
-            if (arr[outer] > arr[inner]){
+            if (arr[outer] > arr[inner])
+            {
+                arr[outer] ^= arr[inner];
+                arr[inner] ^= arr[outer];
+                arr[outer] ^= arr[inner];
+            }
+        }
+    }
+}
+
+void bubble_sort_str(char *arr, size_t nitems)
+{
+    for (int outer = 0; outer < nitems; outer++)
+    {
+        for (int inner = outer + 1; inner < nitems; inner++)
+        {
+            if (arr[outer] > arr[inner])
+            {
                 arr[outer] ^= arr[inner];
                 arr[inner] ^= arr[outer];
                 arr[outer] ^= arr[inner];
@@ -30,9 +47,17 @@ int main(int argc, char *argv[])
 {
     int numbers[] = {7, 2, 3, 6, 1, 4, 0, 9, 8, 5};
 
-    print_arr(numbers);
-    bubble_sort(numbers);
-    print_arr(numbers);
+    print_arr(numbers, 10, sizeof(int));
+    bubble_sort(numbers, 10);
+    print_arr(numbers, 10, sizeof(int));
 
+    // Challenge: Sort string
+    char input[] = "This is a string";
+    printf("String:\t%s\n", input);
+
+    bubble_sort_str(input,strlen(input));
+
+    printf("String:\t%s\n", input);
+    // 
     return 0;
 }
