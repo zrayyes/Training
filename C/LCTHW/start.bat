664f2282e71c799d06c71d01ef6c77ce455ae3d8
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-12-21 10:09:50 +0100

    Updated start.bat

--- C/LCTHW/start.bat
+++ C/LCTHW/start.bat
@@ -1,2 +1,7 @@
-REM START "Docker GCC" docker run -it -d -v C:/Users/Zuhair/Desktop/GitHub/Training/C/LCTHW/exercises:/workdir -w="/workdir" gcc:8\
-START "Docker GCC" docker run -it -d -v C:/Users/rayyes.z/Desktop/GitHub/TrainingCode/C/LCTHW/exercises:/workdir -w="/workdir" gcc:8
+FOR /F "tokens=* USEBACKQ" %%F IN (`whoami`) DO (
+    IF "%%F" == "ik\rayyes.z" (
+        START "Docker GCC" docker run -it -d -v C:/Users/rayyes.z/Desktop/GitHub/TrainingCode/C/LCTHW/exercises:/workdir -w="/workdir" gcc:8
+    ) ELSE (
+        START "Docker GCC" docker run -it -d -v C:/Users/Zuhair/Desktop/GitHub/Training/C/LCTHW/exercises:/workdir -w="/workdir" gcc:8\
+    )
+)
