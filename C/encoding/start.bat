7ffe465cf84c3f575600e7d40299674fc51e585e
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-12-21 11:01:27 +0100

    Updated makefile and start  file

--- C/encoding/start.bat
+++ C/encoding/start.bat
@@ -1,7 +1,7 @@
 FOR /F "tokens=* USEBACKQ" %%F IN (`whoami`) DO (
     IF "%%F" == "ik\rayyes.z" (
-        START "Docker GCC" docker run -it -d -v C:/Users/rayyes.z/Desktop/GitHub/TrainingCode/C/encoding/src:/workdir -w="/workdir" gcc:8
+        START "Docker GCC" docker run -it -d -v C:/Users/rayyes.z/Desktop/GitHub/TrainingCode/C/encoding:/workdir -w="/workdir" gcc:8
     ) ELSE (
-        START "Docker GCC" docker run -it -d -v C:/Users/Zuhair/Desktop/GitHub/Training/C/encoding/src:/workdir -w="/workdir" gcc:8\
+        START "Docker GCC" docker run -it -d -v C:/Users/Zuhair/Desktop/GitHub/Training/C/encoding:/workdir -w="/workdir" gcc:8\
     )
 )
