5fa7f993ee598d3a4e1bd94c7cedf7c5f920345b
Zuhair Rayyes <rayyes.z@ikarus.at>
2019-06-18 15:37:32 +0200

    Chapter 3

--- Python/Flask/hello.py
+++ Python/Flask/hello.py
@@ -1,24 +1,24 @@
-from flask import Flask, request, make_response, redirect, abort
+from flask import Flask, request, make_response, redirect, abort, render_template, url_for
 app = Flask(__name__)
 
 # could also work with app.add_url_rule('/', 'index', index)
 @app.route('/')
 def index():
     user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser is {}</p>'.format(user_agent), 200
+    return render_template("index.html"), 200
 
 @app.route('/user/<name>')
 def user(name):
     if name == "abort":
         abort(404)
-    response = make_response("<h1>Hello, {}</h1>".format(name))
+    response = make_response(render_template("user.html", name=name))
     response.status_code = 200
     return response
 
 @app.route("/redirect")
 def nothing():
-    return redirect("/")
+    return redirect(url_for('index'))
 
-# Could also be started with 
-# if __name__ == "__main__":
-#     app.run()
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
