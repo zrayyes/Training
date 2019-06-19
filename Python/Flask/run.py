345da875ed82ae6883979f1672754336cfc2d49a
Zuhair Rayyes <rayyes.z@ikarus.at>
2019-06-19 11:22:59 +0200

    Added unit tests

--- Python/Flask/run.py
+++ Python/Flask/run.py
@@ -2,3 +2,12 @@ import os
 from app import create_app
 
 app = create_app(os.getenv("FLASK_CONFIG") or "default")
+
+
+@app.cli.command()
+def test():
+    """Run the unit tests"""
+    import unittest
+
+    tests = unittest.TestLoader().discover("tests")
+    unittest.TextTestRunner(verbosity=2).run(tests)
---
+++ Python/Flask/tests/tests.py
@@ -0,0 +1,21 @@
+import unittest
+from flask import current_app
+from app import create_app
+
+
+class BasicsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app("testing")
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        # create db here
+
+    def tearDown(self):
+        self.app_context.pop()
+
+    def test_app_exists(self):
+        self.assertFalse(current_app is None)
+
+    def test_app_is_testing(self):
+        # Checks if testing mode is enabled
+        self.assertTrue(current_app.config["TESTING"])
