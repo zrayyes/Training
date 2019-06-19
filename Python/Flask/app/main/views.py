08337167cbeb3cdf4909db4c7ab5f6f40d6a5bc5
Zuhair Rayyes <rayyes.z@ikarus.at>
2019-06-19 11:08:15 +0200

    Proper file structure

---
+++ Python/Flask/app/main/views.py
@@ -0,0 +1,23 @@
+from flask import render_template, abort, redirect, request, make_response, url_for
+from . import main
+from .. import db
+
+@main.route("/")
+def index():
+    user_agent = request.headers.get("User-Agent")
+    return render_template("index.html"), 200
+
+
+@main.route("/user/<name>")
+def user(name):
+    if name == "abort":
+        abort(404)
+    response = make_response(render_template("user.html", name=name))
+    response.status_code = 200
+    return response
+
+
+@main.route("/redirect")
+def nothing():
+    # namespace.endpoint
+    return redirect(url_for(".index"))
