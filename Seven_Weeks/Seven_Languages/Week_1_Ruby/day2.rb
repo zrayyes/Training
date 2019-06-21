526beba1da10f8b0780a5f265b84d918c7894a3d
Zuhair Rayyes <rayyes.z@ikarus.at>
2019-06-21 10:50:28 +0200

    day 2 done

--- Seven_Weeks/Seven_Languages/Week_1_Ruby/day2.rb
+++ Seven_Weeks/Seven_Languages/Week_1_Ruby/day2.rb
@@ -1,15 +1,19 @@
-# No code block
+# Read file without code block
 file = File.open("dummy.txt","r")
 for line in file
     puts line
 end
 
-# With code block
+# Read file with code block
 File.open("dummy.txt","r") {|file| file.each {|line| puts line}}
 
-
+# Hash to array
 hash = {"prop1": "key1", "prop2": "key2"}
-puts hash.flatten()
+p hash.flatten()
 
+# Array to hash
 arr = [["p1","k1"], ["p2", "k2"]]
-puts arr.to_h
+p arr.to_h
+
+# Array to array slices
+(1..16).each_slice(4) { |a| p a }
