8870f73fa03cc5a8a32524833ad62d7d54c8a33e
Zuhair Rayyes <rayyes95@gmail.com>
2019-06-23 18:16:19 +0200

    Added a "forbidden" route

--- Python/Flask_REST/app/api/users.py
+++ Python/Flask_REST/app/api/users.py
@@ -1,5 +1,10 @@
 from . import api
+from .errors import forbidden
 
 @api.route("/users/all")
 def index():
     return "hello"
+
+@api.route("users/secret")
+def secret():
+    return forbidden("This is a sekrut")
